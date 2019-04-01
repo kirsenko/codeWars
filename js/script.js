99f2ffc8104a84d61df69b490a9c3e759852a62b
Student <student@lan.itstep.kiev.ua>
2019-04-01 19:41:38 +0300

    First comment

---
+++ js/script.js
@@ -0,0 +1,220 @@
+"use strict";
+
+// 1---------------------------------
+
+/*Implement the function unique_in_order which takes as argument a sequence and returns a list
+ of items without any elements with the same value next to each other and preserving the original
+ order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD') == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1, 2, 2, 3, 3]) == [1, 2, 3]
+*/
+var uniqueInOrder = function(iterable) {
+  //your code here - remember iterable can be a string or an array
+  if (typeof iterable === "string") {
+    iterable = iterable.split("");
+  }
+  let newArr = iterable.reduce(function(acc, item, i) {
+    if (item != iterable[i + 1]) {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
+  return newArr;
+};
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3, 5, 6, 7, 8, 8, 8, 2, 2, 3, 3, 3, 5, 5, 5, 6]));
+
+
+
+var uniqueInOrder1 = function (iterable) {
+  return [...iterable].filter((a, i) => a !== iterable[i - 1])
+}
+console.log(uniqueInOrder1("AAAABBBCCDAABBB"));
+
+
+
+var uniqueInOrder3 = function (iterable) {
+  return Array.from(iterable).reduce((res, cur, index) => {
+    if (res[res.length - 1] !== cur) {
+      res.push(cur);
+    }
+    return res;
+  }, [])
+}
+console.log(uniqueInOrder3("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder3([1, 2, 2, 3, 3, 5, 6, 7, 8, 8, 8, 2, 2, 3, 3, 3, 5, 5, 5, 6]));
+
+// 2---------------------------------------
+
+/*Create a function named divisors / Divisors that takes an integer n > 1 and returns an array
+ with all of the integer's divisors(except for 1 and the number itself), from smallest to largest.
+  If the number is prime return the string '(integer) is prime' (null in C#)
+   (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+*/
+
+function divisors(integer) {
+  let arr =[];
+  for (let i = 2; i < integer; i++) {
+  if (!(integer%i)) {
+    arr.push(i);
+  }
+  } if (arr.length) {
+    return arr;
+  } else return integer + ' is prime';
+};
+console.log(divisors(12));
+
+
+
+function divisors1(integer) {
+  let divisors = [...Array(integer).keys()].slice(2, integer).filter(div => integer % div == 0);
+  return divisors.length > 0 ? divisors : integer + ' is prime';
+};
+console.log(divisors1(12));
+
+// 3--------------------------------
+/*
+Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
+
+Let's assume that a song consists of some number of words (that don't contain WUB). To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
+
+For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
+
+Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed. Help Jonny restore the original song.
+
+Input
+The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
+
+Output
+Return the words of the initial song that Polycarpus used to make a dubsteb remix. Separate the words with a space.
+
+Examples
+songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
+  // =>  WE ARE THE CHAMPIONS MY FRIEND
+
+*/
+
+
+function songDecoder(song) {
+   let arr = song.split('WUB').reduce(function (acc, item) {
+    if (item) {
+      acc.push(item);
+    }return acc;
+  }, []);
+  return arr.join(' ');
+}
+console.log(songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"));
+
+
+
+function songDecoder1(song) {
+  return song.split('WUB').filter(Boolean).join(' ');
+}
+console.log(songDecoder1("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"));
+
+// 4---------------------------------------
+/*
+The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
+
+Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
+
+Return YES, if Vasya can sell a ticket to each person and give the change with the bills he has at hand at that moment. Otherwise return NO.
+
+Examples:
+tickets([25, 25, 50]) // => YES
+tickets([25, 100]) // => NO. Vasya will not have enough money to give change to 100 dollars
+tickets([25, 25, 50, 50, 100]) // => NO. Vasya will not have the right bills to give 75 dollars of ch
+
+*/
+
+// 5
+/*
+Given an array of one's and zero's convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+*/
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2);
+};
+console.log(binaryArrayToNumber([1, 0, 1, 1]));
+
+// 6
+
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.
+
+array_diff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+array_diff([1,2,2,2,3],[2]) == [1,3]
+
+*/
+
+function array_diff(a, b) {
+  let arr = [];
+  for (let key in a) {
+    if (!(b.includes(a[key]))) {
+      arr.push(a[key]);
+    }
+  } return arr;
+  }
+console.log(array_diff([1, 2, 2, 2, 3], [2]));
+
+
+function array_diff1(a, b) {
+  return a.filter(function (x) { return b.indexOf(x) == -1; });
+}
+console.log(array_diff1([1, 2, 2, 2, 3], [2]));
+
+function array_diff2(a, b) {
+  return a.filter(e => !b.includes(e));
+}
+console.log(array_diff2([1, 2, 2, 2, 3], [2]));
+
+// 7
+/*
+Цель этого упражнения - преобразовать строку в новую строку, где каждый символ в новой строке имеет место,
+"("если этот символ появляется только один раз в исходной строке или ")"если этот символ появляется более
+одного раза в исходной строке. Игнорировать заглавные буквы при определении, является ли персонаж дубликатом.
+
+Примеры
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
+
+*/
+
+function duplicateEncode(word) {
+  // ...
+  word = word.toUpperCase();
+ const arr = word.split('');
+  console.log(arr);
+}
+duplicateEncode('Success');
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function duplicateEncode(word) {
+  // ...
+  word = word.toUpperCase();
+ const arr = word.split('');
+  console.log(arr);
+}
+duplicateEncode('Success');
