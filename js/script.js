be2d82ecdab8abb5a7c142c79ba15a047790954b
kirsenko <kirsenko8000@gmail.com>
2019-04-28 07:16:56 +0300

    task 10

--- js/script.js
+++ js/script.js
@@ -309,5 +309,9 @@ function pigIt(str) {
 
 }
 
+function pigIt(str) {
+  return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
+}
+
 pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 pigIt('Hello world !'); // elloHay orldway !
